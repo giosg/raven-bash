c146e207c599b734df9a35f28008ebaae4f946a5
Mikael Taina <keras@users.noreply.github.com>
2017-03-28 14:03:36 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='raven-bash',
-    version='0.1.0',
+    version='0.1.1',
     description='Raven Sentry client for Bash.',
     classifiers=[],
     keywords='raven sentry bash',
